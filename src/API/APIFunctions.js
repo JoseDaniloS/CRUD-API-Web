395626cafda9903229d54bff33c3ac601c05eca3
Jose Danilo <danilojose.1d@gmail.com>
2024-12-29 15:34:26 -0300

    fix: api_url in APIFunctions.js

--- src/API/APIFunctions.js
+++ src/API/APIFunctions.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import {toast } from "react-toastify";
 
-const API_URL = "http://localhost:3000";
+const API_URL = "https://crud-api3.onrender.com";
 
 
 // Function to get all news
